4d9e7d97577e05d06fe8e44365f06805dc586894
CodiSordelet <codisordelet@Codis-MacBook-Pro.local>
2019-03-09 23:10:48 -0500

    Save changes
    Save

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import BootstrapTable from 'react-bootstrap-table-next';
+import { Type } from 'react-bootstrap-table2-editor';
+
+
+
+
 class App extends Component {
 
   state = {
+    modalShow: false,
     data: [
       {
+        key:0,
         companyName: 'Integra llc',
         companyLocation: 'Atlanta, GA',
         currentValuation: 60000000,
@@ -14,14 +21,15 @@ class App extends Component {
           name: 'Codi Sordelet',
           position:'CEO',
         }],
-        status:'approved',
+        status:'Pending Approval',
         grossProfitMargin: 7575884,
         netProfit:67588548,
         netProfitMargin: 6778585,
         currentRatio: .6
       },
       {
-        companyName: 'Integra llc',
+        key: 1,
+        companyName: 'Alan llc',
         companyLocation: 'Atlanta, GA',
         currentValuation: 60000000,
         companyDescription:'',
@@ -36,7 +44,8 @@ class App extends Component {
         currentRatio: .6
       },
       {
-        companyName: 'Integra llc',
+        key: 2,
+        companyName: 'Radiant llc',
         companyLocation: 'Atlanta, GA',
         currentValuation: 60000000,
         companyDescription:'',
@@ -51,7 +60,8 @@ class App extends Component {
         currentRatio: .6
       },
       {
-        companyName: 'Integra llc',
+        key: 3,
+        companyName: 'Cramer llc',
         companyLocation: 'Atlanta, GA',
         currentValuation: 60000000,
         companyDescription:'',
@@ -62,7 +72,8 @@ class App extends Component {
         currentRatio: .6
       },
       {
-        companyName: 'Integra llc',
+        key: 4,
+        companyName: 'Giovanni\'s Pizza',
         companyLocation: 'Atlanta, GA',
         currentValuation: 60000000,
         companyDescription:'',
@@ -77,7 +88,8 @@ class App extends Component {
         currentRatio: .6
       },
       {
-        companyName: 'Integra llc',
+        key: 5,
+        companyName: 'Boda Inc',
         companyLocation: 'Atlanta, GA',
         currentValuation: 60000000,
         companyDescription:'',
@@ -90,16 +102,87 @@ class App extends Component {
         netProfit:67588548,
         netProfitMargin: 6778585,
         currentRatio: .6
-      },
-    ]
+      }
+    ],
+    selectedRows: []
   }
 
-  componentDidMount() {
-    
+
+
+  onClickOfRow = (e, row, rowIndex) => {
+    let selectedRows=[...this.state.selectedRows];
+
+    if(selectedRows.includes(row.key)) {
+      selectedRows = selectedRows.filter(value => value !== row.key);
+    } else if(!selectedRows.includes(row.key)) {
+      selectedRows.push(row.key);
+    }
+
+    this.setState({selectedRows});
   }
+
+
+  handleDelete = () => {
+   let rows = [...this.state.data];
+    /*
+    * Delete should filter for each
+    *
+    * 
+    */ 
+
+    // for(let i = 0; this.state.selectedRows.length; i++) {
+    //   const key = this.state.selectedRows[i];
+    //   console.log(rows.includes(key));
+    //   if(rows.includes(key)) {
+
+    //       rows = rows.filter(row => row.key !== key);
+    //   }
+    // }
+
+    this.setState({data: rows});
+    // let arrayOfRows = [...this.state.selectedRows];
+    // //
+    // let dataArray = [...this.state.data];
+    // for(let i = 0; i < arrayOfRows.length; i++) {
+      
+    //   delete dataArray[temp];
+    // }
+    
+    // let finalArray = this.arrayRemove(dataArray,'undefined');
+    // console.log(finalArray);
+    //  this.setState({
+    //    data: finalArray,
+    //    selectedRows: []
+    //   });
+  }
+
+  handleOnSelect = (row, isSelect) => {
+    return true; 
+  }
+
+  handleOnSelectAll = (isSelect, rows) => {
+    if (isSelect && rows) {
+      let selectedRows = rows.map(r => r.key);
+      this.setState({selectedRows});
+
+    } else if(isSelect && !rows) {
+      
+      this.setState({selectedRows: []});
+  
+    }
+  }
+
+  showModal = () => {
+    this.setState({showModal: true})
+  }
+
+  addNewRow = () => {
+
+  }
+
 
   render() {
-
+    const data =[...this.state.data];
     const columns = [{
       dataField: 'companyName',
       text: 'Company Name'
@@ -116,7 +199,23 @@ class App extends Component {
     },
     {
       dataField: 'status',
-      text: 'Status'
+      text: 'Status',
+      editor: {
+        type: Type.SELECT,
+        options: [{
+          value: 'Resesarching',
+          label: 'Resesarching'
+        }, {
+          value: 'Pending Approval',
+          label: 'Pending Approval'
+        }, {
+          value: 'Approved',
+          label: 'Approved'
+        }, {
+          value: 'Declined',
+          label: 'Declined'
+        }]
+      }
     },
     {
       dataField: 'grossProfitMargin',
@@ -133,18 +232,43 @@ class App extends Component {
     {
       dataField: 'currentRatio',
       text: 'Current Ratio'
-    }
+    },
+    {
+      dataField: "databasePkey",
+      text: "Remove",
+      
+  }
   ];
 
-  const rowStyle = { backgroundColor: '#c8e6c9' };
+  const rowStyle = {
+     backgroundColor: 'white',
+     fontSize: 12
+  };
 
+  const rowEvents = {
+    onClick: this.onClickOfRow
+  };
 
-   
-    
+  const selectRow = {
+    mode: 'checkbox',
+    clickToSelect: true,
+    style: { backgroundColor: '#ef8383' },
+    onSelect: this.handleOnSelect,
+    onSelectAll: this.handleOnSelectAll
+  };
+
 
     return (
       <div className="App">
-       <BootstrapTable keyField='id' data={ this.state.data } columns={ columns } rowStyle={ rowStyle } />
+      <button type="button" className="btn btn-primary" onClick={this.addNew}>Add New</button><br/>
+      <button type="button" className="btn btn-danger" onClick={this.handleDelete}>Delete Row(s)</button>
+        <BootstrapTable 
+        keyField='key' 
+        data={ data } 
+        columns={ columns } 
+        rowEvents={rowEvents} 
+        rowStyle={rowStyle} 
+        selectRow={selectRow} />
       </div>
     );
   }
